a1282a09793dede90ad75ef10b1af5e01b2b1a4a
Saerom Park <park.srome@gmail.com>
2022-06-07 15:41:39 -0500

    google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
+import ReactGA from 'react-ga'; 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  ReactGA.initialize('UA-231145616-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          we live
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
