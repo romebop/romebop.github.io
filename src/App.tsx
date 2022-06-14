985672fdb35cd3809d380136bda5415da24d0e20
Saerom Park <park.srome@gmail.com>
2022-06-14 08:02:16 -0500

    ga tag in head

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React, { useEffect } from 'react';
-import ReactGA from 'react-ga'; 
- 
 import drifter from './assets/drifter.gif';
 import './App.css';
 
 function App() {
-  
-  useEffect(() => {
-    ReactGA.initialize('UA-231944724-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
