68637d55461bafacf03e6a29c8b9e0130ca48b44
Saerom Park <park.srome@gmail.com>
2022-06-14 07:07:34 -0500

    react-ga useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactGA from 'react-ga'; 
+ 
 import drifter from './assets/drifter.gif';
 import './App.css';
 
 function App() {
+  
+  console.log('app init');
+  console.log(window.location.pathname + window.location.search);
 
-  ReactGA.initialize('UA-231145616-1');
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  useEffect(() => {
+    ReactGA.initialize('UA-231145616-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <div className="App">
