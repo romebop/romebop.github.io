9be7e8e60ed4b957b6aa285d74afe78453d6b1c5
Saerom Park <park.srome@gmail.com>
2022-06-17 16:53:07 -0500

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,57 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 import drifter from './assets/drifter.gif';
 import './App.css';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={drifter} alt="drifter" />
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">About</Link>
+                </li>
+                <li>
+                  <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                  <Link to="/visualizations">Users</Link>
+                </li>
+              </ul>
+            </nav>
+            <Routes>
+              <Route path="/" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/visualizations" element={<Visualizations />} />
+            </Routes>
+          </div>
+        </Router>
       </header>
     </div>
   );
 }
+
+function About() {
+  return <img src={drifter} alt="drifter" />;
+}
+
+function Projects() {
+  return <h2>Projects</h2>;
+}
+
+function Visualizations() {
+  return <h2>Visualizations</h2>;
+}
+
 
 export default App;
