ecab2486841081de3efff4b61002e62750042bf0
Saerom Park <park.srome@gmail.com>
2024-01-07 22:07:26 -0600

    tune perlin noise

--- src/components/CoolThing.tsx
+++ src/components/CoolThing.tsx
@@ -24,11 +24,19 @@ const CoolThing: FC = () => {
       let xOff = 0;
       let yOff = 0;
       let zOff = 0;
-      const xyInc = 0.25; // how fine is the detail (higher is zoom out)
+      const xyInc = 0.14; // how fine is the detail (higher is zoom out)
       const zInc = 0.0003; // how fast does it change (higher is more rapid)
+      const exp = 4; // how blobby (higher is more blobby)
+
+      let start = 0;
+      const startInc = 0.0001;
 
       p.setup = () => {
         p.createCanvas(width + strokeWidth, height + strokeWidth);
+        p.strokeWeight(strokeWidth);
+        p.strokeCap(p.SQUARE);
+        p.stroke('white');
+        p.noFill();
         p.frameRate(100);
       };
 
@@ -43,26 +51,14 @@ const CoolThing: FC = () => {
         }
       };
 
-      p.draw = () => {
-        
-        console.log('drawing now');
-
-        p.background(0);
-        p.strokeWeight(strokeWidth);
-        p.strokeCap(p.SQUARE);
-        p.stroke('white');
-        p.noFill();
-
-        drawGrid();
-
-        xOff = 0;
+      const drawShapes = () => {
+        xOff = start;
         for (let i = 0; i < rows; i++) { // height
-          yOff = 0;
+          yOff = -start;
           for (let j = 0; j < cols; j++) { // width
             const noiseVal = p.noise(xOff, yOff, zOff);
-            const powVal = p.pow(noiseVal, 3);
-            const mappedVal = p.map(powVal, 0, 1, 0, 80, true);
-            // const roundedVal = p.round(mappedVal);
+            const powVal = p.pow(noiseVal, exp);
+            // const roundedVal = powVal.toFixed(3);
 
             // const x = cellWidth * j + cellWidth / 2;
             // const y = cellHeight * i + cellHeight / 2;
@@ -70,17 +66,20 @@ const CoolThing: FC = () => {
             // text
             // p.textAlign(p.CENTER, p.CENTER);
             // p.strokeWeight(0.5);
-            // p.textSize(4 + (roundedVal * 6));
+            // p.textSize(16);
             // p.fill('white');
             // p.text(roundedVal, x,  y);
 
             // circle
             // p.fill('white');
-            // p.ellipse(x, y, mappedVal);
+            // p.ellipse(x, y, powVal * 200);
 
-            console.log(mappedVal);
-
-            if (10 < mappedVal && mappedVal <= 14) { // line
+            const bp1 = 0.10;
+            const bp2 = 0.12;
+            const bp3 = 0.14;
+            const bp4 = 0.18;
+            const bp5 = 0.22;
+            if (bp1 < powVal && powVal <= bp2) { // line
               p.line(
                 strokeWidth + (cellWidth * j),
                 (strokeWidth / 2) + (cellHeight * i) + (cellHeight / 2),
@@ -88,13 +87,13 @@ const CoolThing: FC = () => {
                 (strokeWidth / 2) + (cellHeight * i) + (cellHeight / 2)
               );
             }
-            if (14 < mappedVal && mappedVal <= 18) { // circle
+            if (bp2 < powVal && powVal <= bp3) { // circle
               const circleX = (strokeWidth / 2) + (cellWidth * j) + (cellWidth / 2);
               const circleY = (strokeWidth / 2) + (cellHeight * i) + (cellHeight / 2);
               const radius = Math.min(cellWidth, cellHeight) / 2 - (strokeWidth * 2);
               p.ellipse(circleX, circleY, radius * 2);
             }
-            if (18 < mappedVal && mappedVal <= 22) { // x
+            if (bp3 < powVal && powVal <= bp4) { // x
               p.line(
                 (strokeWidth / 2) + (cellWidth * j),
                 (strokeWidth / 2) + (cellHeight * i),
@@ -108,7 +107,7 @@ const CoolThing: FC = () => {
                 (strokeWidth / 2) + (cellHeight * i)
               );
             }
-            if (22 < mappedVal && mappedVal <= 26) { // tic-tac-toe
+            if (bp4 < powVal && powVal <= bp5) { // tic-tac-toe
               p.line(
                 strokeWidth + (cellWidth * j),
                 (strokeWidth / 2) + (cellHeight * i) + (cellHeight / 3),
@@ -134,7 +133,7 @@ const CoolThing: FC = () => {
                 (cellHeight * i) + cellHeight
               );
             }
-            if (26 < mappedVal) { // stripes
+            if (bp5 < powVal) { // stripes
               p.line(
                 (strokeWidth / 2) + (cellWidth * j) + (cellWidth / 9),
                 strokeWidth + (cellHeight * i),
@@ -189,6 +188,13 @@ const CoolThing: FC = () => {
           xOff += xyInc;
           zOff += zInc;
         }
+        start += startInc;
+      }
+
+      p.draw = () => {
+        p.background(0);
+        drawGrid();
+        drawShapes();
       };
       
     };
